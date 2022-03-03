def9f4fd506d9360818a600016da02bcb499d8e8
Kyran Flynn <kyran_flynn@brown.edu>
2022-03-02 23:29:06 -0500

    Six cryptocurrencies

--- data_deliverable/code/coin_cleaning.py
+++ data_deliverable/code/coin_cleaning.py
@@ -1,4 +1,3 @@
-''' This file cleans and combines the raw coin data using a pandas dataframe '''
 import os
 import pandas as pd
 import json
@@ -6,6 +5,9 @@ import json
 # Finds root directory of user
 ROOT_DIR = os.path.dirname(os.path.abspath((__file__)))
 
+''' Here we collect the data for three of the top 'proper' cryptocurrencies on the market (Bitcoin, Ethereum, Solana) '''
+
+''' Bitcoin '''
 # All five raw BTC data filepaths
 BTC_MarToApr = '/../data/raw/BTC(1)_MarToApr.json'
 BTC_AprToJun = '/../data/raw/BTC(2)_AprToJun.json'
@@ -33,6 +35,66 @@ btc_data = btc_data_1['Data']['Data'] + btc_data_2['Data']['Data'] + btc_data_3[
         + btc_data_4['Data']['Data'] + btc_data_5['Data']['Data']
 btc_df = pd.DataFrame.from_records(btc_data, columns=['time', 'high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
 
+
+''' Ethereum '''
+# All five raw ETH data filepaths
+ETH_MarToApr = '/../data/raw/ETH(1)_MarToApr.json'
+ETH_AprToJun = '/../data/raw/ETH(2)_AprToJun.json'
+ETH_JunToSep = '/../data/raw/ETH(3)_JunToSep.json'
+ETH_SepToDec = '/../data/raw/ETH(4)_SepToDec.json'
+ETH_DecToMar = '/../data/raw/ETH(5)_DecToMar.json'
+
+# ETH records collected and merged
+file = open(ROOT_DIR + ETH_MarToApr, 'r')
+eth_data_1 = json.load(file)
+file.close()
+file = open(ROOT_DIR + ETH_AprToJun, 'r')
+eth_data_2 = json.load(file)
+file.close()
+file = open(ROOT_DIR + ETH_JunToSep, 'r')
+eth_data_3 = json.load(file)
+file.close()
+file = open(ROOT_DIR + ETH_SepToDec, 'r')
+eth_data_4 = json.load(file)
+file.close()
+file = open(ROOT_DIR + ETH_DecToMar, 'r')
+eth_data_5 = json.load(file)
+file.close()
+eth_data = eth_data_1['Data']['Data'] + eth_data_2['Data']['Data'] + eth_data_3['Data']['Data'] \
+        + eth_data_4['Data']['Data'] + eth_data_5['Data']['Data']
+eth_df = pd.DataFrame.from_records(eth_data, columns=['high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
+
+''' Solana '''
+# All five raw SOL data filepaths
+SOL_MarToApr = '/../data/raw/SOL(1)_MarToApr.json'
+SOL_AprToJun = '/../data/raw/SOL(2)_AprToJun.json'
+SOL_JunToSep = '/../data/raw/SOL(3)_JunToSep.json'
+SOL_SepToDec = '/../data/raw/SOL(4)_SepToDec.json'
+SOL_DecToMar = '/../data/raw/SOL(5)_DecToMar.json'
+
+# SOL records collected and merged
+file = open(ROOT_DIR + SOL_MarToApr, 'r')
+sol_data_1 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SOL_AprToJun, 'r')
+sol_data_2 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SOL_JunToSep, 'r')
+sol_data_3 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SOL_SepToDec, 'r')
+sol_data_4 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SOL_DecToMar, 'r')
+sol_data_5 = json.load(file)
+file.close()
+sol_data = sol_data_1['Data']['Data'] + sol_data_2['Data']['Data'] + sol_data_3['Data']['Data'] \
+        + sol_data_4['Data']['Data'] + sol_data_5['Data']['Data']
+sol_df = pd.DataFrame.from_records(sol_data, columns=['high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
+
+''' Here we collect the data for three of the top 'meme' cryptocurrencies on the market (Dogecoin, Shiba Inu, Sushi)'''
+
+''' Dogecoin '''
 # All five raw DOGE data filepaths are defined here
 DOGE_MarToApr = '/../data/raw/DOGE(1)_MarToApr.json'
 DOGE_AprToJun = '/../data/raw/DOGE(2)_AprToJun.json'
@@ -58,10 +120,70 @@ doge_data_5 = json.load(file)
 file.close()
 doge_data = doge_data_1['Data']['Data'] + doge_data_2['Data']['Data'] + doge_data_3['Data']['Data'] \
         + doge_data_4['Data']['Data'] + doge_data_5['Data']['Data']
+
+
+''' Shiba Inu '''
+# All five raw SHIB data filepaths are defined here
+SHIB_MarToApr = '/../data/raw/SHIB(1)_MarToApr.json'
+SHIB_AprToJun = '/../data/raw/SHIB(2)_AprToJun.json'
+SHIB_JunToSep = '/../data/raw/SHIB(3)_JunToSep.json'
+SHIB_SepToDec = '/../data/raw/SHIB(4)_SepToDec.json'
+SHIB_DecToMar = '/../data/raw/SHIB(5)_DecToMar.json'
+
+# SHIB records collected and merged
+file = open(ROOT_DIR + SHIB_MarToApr, 'r')
+shib_data_1 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SHIB_AprToJun, 'r')
+shib_data_2 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SHIB_JunToSep, 'r')
+shib_data_3 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SHIB_SepToDec, 'r')
+shib_data_4 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SHIB_DecToMar, 'r')
+shib_data_5 = json.load(file)
+file.close()
+shib_data = shib_data_1['Data']['Data'] + shib_data_2['Data']['Data'] + shib_data_3['Data']['Data'] \
+        + shib_data_4['Data']['Data'] + shib_data_5['Data']['Data']
+
+
+''' Sushi '''
+# All five raw SUSHI data filepaths are defined here
+SUSHI_MarToApr = '/../data/raw/SUSHI(1)_MarToApr.json'
+SUSHI_AprToJun = '/../data/raw/SUSHI(2)_AprToJun.json'
+SUSHI_JunToSep = '/../data/raw/SUSHI(3)_JunToSep.json'
+SUSHI_SepToDec = '/../data/raw/SUSHI(4)_SepToDec.json'
+SUSHI_DecToMar = '/../data/raw/SUSHI(5)_DecToMar.json'
+
+# SUSHI records collected and merged
+file = open(ROOT_DIR + SUSHI_MarToApr, 'r')
+sushi_data_1 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SUSHI_AprToJun, 'r')
+sushi_data_2 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SUSHI_JunToSep, 'r')
+sushi_data_3 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SUSHI_SepToDec, 'r')
+sushi_data_4 = json.load(file)
+file.close()
+file = open(ROOT_DIR + SUSHI_DecToMar, 'r')
+sushi_data_5 = json.load(file)
+file.close()
+sushi_data = sushi_data_1['Data']['Data'] + sushi_data_2['Data']['Data'] + sushi_data_3['Data']['Data'] \
+        + sushi_data_4['Data']['Data'] + sushi_data_5['Data']['Data']
  
 # BTC and DOGE data converted to a dataframe with selected columns (size: 8761x7)
 btc_df = pd.DataFrame.from_records(btc_data, columns=['time', 'high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
+eth_df = pd.DataFrame.from_records(eth_data, columns=['high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
+sol_df = pd.DataFrame.from_records(sol_data, columns=['high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
 doge_df = pd.DataFrame.from_records(doge_data, columns=['high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
+shib_df = pd.DataFrame.from_records(shib_data, columns=['high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
+sushi_df = pd.DataFrame.from_records(sushi_data, columns=['high', 'low', 'open', 'volumefrom', 'volumeto', 'close'])
 
 # Convert UNIX timestamp unit to date unit
 btc_df['time'] = pd.to_datetime(btc_df['time'],unit='s')
@@ -69,11 +191,19 @@ btc_df['time'] = pd.to_datetime(btc_df['time'],unit='s')
 # Add crytpocurrency label to column names for dataframe merge
 btc_df.rename(columns={'high': 'btc_high', 'low': 'btc_low', 'open': 'btc_open', 'volumefrom': 'btc_volumefrom', \
     'volumeto': 'btc_volumeto', 'close': 'btc_close'}, inplace=True)
+eth_df.rename(columns={'high': 'eth_high', 'low': 'eth_low', 'open': 'eth_open', 'volumefrom': 'eth_volumefrom', \
+    'volumeto': 'eth_volumeto', 'close': 'eth_close'}, inplace=True)
+sol_df.rename(columns={'high': 'sol_high', 'low': 'sol_low', 'open': 'sol_open', 'volumefrom': 'sol_volumefrom', \
+    'volumeto': 'sol_volumeto', 'close': 'sol_close'}, inplace=True)
 doge_df.rename(columns={'high': 'doge_high', 'low': 'doge_low', 'open': 'doge_open', 'volumefrom': 'doge_volumefrom', \
     'volumeto': 'doge_volumeto', 'close': 'doge_close'}, inplace=True)
+shib_df.rename(columns={'high': 'shib_high', 'low': 'shib_low', 'open': 'shib_open', 'volumefrom': 'shib_volumefrom', \
+    'volumeto': 'shib_volumeto', 'close': 'shib_close'}, inplace=True)
+sushi_df.rename(columns={'high': 'sushi_high', 'low': 'sushi_low', 'open': 'sushi_open', 'volumefrom': 'sushi_volumefrom', \
+    'volumeto': 'sushi_volumeto', 'close': 'sushi_close'}, inplace=True)
 
 # Merge the dataframes
-coin_df = btc_df.join(doge_df)
+coin_df = btc_df.join([eth_df, sol_df, doge_df, shib_df, sushi_df])
 
 # Check that data is clean (missing entries, NaN in df)
 if coin_df.isnull().values.any():
--- data_deliverable/code/coin_scraping.py
+++ data_deliverable/code/coin_scraping.py
@@ -1,17 +1,23 @@
-''' This file retrieves one year of hour to hour Bitcoin and Dogecoin data from the CryptoCompare API '''
 import requests
 import json
+import os
 
 # Key ready to append to CryptoCompare link
 API_key = '&api_key=b7c3016be1c1ed1846f62faf8c67b93b05ef0ec4122a489cda37945114a3e8b1'
 
+# Finds root directory of user
+ROOT_DIR = os.path.dirname(os.path.abspath((__file__)))
+
 ''' 
 Due to the limit of 2000 hours of historical data per request, the year is separated into five intervals. The toTs
-URL parameter sets the endpoint of the interval request, expressed by a UNIX Timestamp. The conversion from dates to
+URL parameter sets the endpoint of the interval requested, expressed in the UNIX Timestamp unit. The conversion from dates to
 UNIX timestamps were made using this website: https://www.epochconverter.com. The data is from 12:00 AM on March 1st 
-2021 to 12:00 AM on March 1st 2022 inclusive for UTC-05:00 (EST).
+2021 to 12:00 AM on March 1st 2022 (inclusive) for UTC-05:00 (EST), and covers three of some of the top 'proper'
+cryptocurrencies (Bictoin, Ethereum, Solana) and three of some of the top 'meme' cryptocurrencies (Dogecoin, Shiba Inu,
+Sushi) by market capitalization.
 '''
 
+''' Bitcoin '''
 # Links for BTC (Bitcoin) historical data
 BTC_MarToApr = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=BTC&tsym=USD&limit=756&toTs=1617296400'
 BTC_AprToJun = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=BTC&tsym=USD&limit=2000&toTs=1624500000'
@@ -23,33 +29,115 @@ BTC_DecToMar = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=BTC&tsy
 # REQUEST 1: March 1, 2021 - April 1, 2021 (1:00:00 PM)
 r = requests.get(BTC_MarToApr + API_key)
 r = r.json()
-with open('BTC(1)_MarToApr.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/BTC(1)_MarToApr.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 2: Thursday, April 1, 2021 (1:00:00 PM) - Wednesday, June 23, 2021 (10:00:00 PM)
 r = requests.get(BTC_AprToJun + API_key)
 r = r.json()
-with open('BTC(2)_AprToJun.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/BTC(2)_AprToJun.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 3: Wednesday, June 23, 2021 (3:00:00 PM) - Wednesday, September 15, 2021 (7:00:00 AM)
 r = requests.get(BTC_JunToSep + API_key)
 r = r.json()
-with open('BTC(3)_JunToSep.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/BTC(3)_JunToSep.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 4: Wednesday, September 15, 2021 (7:00:00 AM) - Tuesday, December 7, 2021 (3:00:00 PM)
 r = requests.get(BTC_SepToDec + API_key)
 r = r.json()
-with open('BTC(4)_SepToDec.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/BTC(4)_SepToDec.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 5: Tuesday December 7 2021 (3:00:00 PM) - Tuesday March 1 2022 (12:00:00 AM)
 r = requests.get(BTC_DecToMar + API_key)
 r = r.json()
-with open('BTC(5)_DecToMar.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/BTC(5)_DecToMar.json','w') as json_file:
     json.dump(r,json_file)
 
+
+''' Ethereum '''
+# Links for ETH (Ethereum) historical data
+ETH_MarToApr = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=756&toTs=1617296400'
+ETH_AprToJun = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=2000&toTs=1624500000'
+ETH_JunToSep = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=2000&toTs=1631703600'
+ETH_SepToDec = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=2000&toTs=1638907200'
+ETH_DecToMar = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=ETH&tsym=USD&limit=2000&toTs=1646110800'
+
+# The five requests for ETH data are made here, and outputted as json files
+# REQUEST 1: March 1, 2021 - April 1, 2021 (1:00:00 PM)
+r = requests.get(ETH_MarToApr + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/ETH(1)_MarToApr.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 2: Thursday, April 1, 2021 (1:00:00 PM) - Wednesday, June 23, 2021 (10:00:00 PM)
+r = requests.get(ETH_AprToJun + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/ETH(2)_AprToJun.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 3: Wednesday, June 23, 2021 (3:00:00 PM) - Wednesday, September 15, 2021 (7:00:00 AM)
+r = requests.get(ETH_JunToSep + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/ETH(3)_JunToSep.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 4: Wednesday, September 15, 2021 (7:00:00 AM) - Tuesday, December 7, 2021 (3:00:00 PM)
+r = requests.get(ETH_SepToDec + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/ETH(4)_SepToDec.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 5: Tuesday December 7 2021 (3:00:00 PM) - Tuesday March 1 2022 (12:00:00 AM)
+r = requests.get(ETH_DecToMar + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/ETH(5)_DecToMar.json','w') as json_file:
+    json.dump(r,json_file)
+
+
+''' Solana '''
+# Links for SOL (Solana) historical data
+SOL_MarToApr = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SOL&tsym=USD&limit=756&toTs=1617296400'
+SOL_AprToJun = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SOL&tsym=USD&limit=2000&toTs=1624500000'
+SOL_JunToSep = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SOL&tsym=USD&limit=2000&toTs=1631703600'
+SOL_SepToDec = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SOL&tsym=USD&limit=2000&toTs=1638907200'
+SOL_DecToMar = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SOL&tsym=USD&limit=2000&toTs=1646110800'
+
+# The five requests for SOL data are made here, and outputted as json files
+# REQUEST 1: March 1, 2021 - April 1, 2021 (1:00:00 PM)
+r = requests.get(SOL_MarToApr + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SOL(1)_MarToApr.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 2: Thursday, April 1, 2021 (1:00:00 PM) - Wednesday, June 23, 2021 (10:00:00 PM)
+r = requests.get(SOL_AprToJun + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SOL(2)_AprToJun.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 3: Wednesday, June 23, 2021 (3:00:00 PM) - Wednesday, September 15, 2021 (7:00:00 AM)
+r = requests.get(SOL_JunToSep + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SOL(3)_JunToSep.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 4: Wednesday, September 15, 2021 (7:00:00 AM) - Tuesday, December 7, 2021 (3:00:00 PM)
+r = requests.get(SOL_SepToDec + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SOL(4)_SepToDec.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 5: Tuesday December 7 2021 (3:00:00 PM) - Tuesday March 1 2022 (12:00:00 AM)
+r = requests.get(SOL_DecToMar + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SOL(5)_DecToMar.json','w') as json_file:
+    json.dump(r,json_file)
+
+
+''' Dogecoin '''
 # Links for DOGE (Dogecoin) historical data
 DOGE_MarToApr = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=DOGE&tsym=USD&limit=756&toTs=1617296400'
 DOGE_AprToJun = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=DOGE&tsym=USD&limit=2000&toTs=1624500000'
@@ -61,29 +149,109 @@ DOGE_DecToMar = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=DOGE&t
 # REQUEST 1: March 1, 2021 - April 1, 2021 (1:00:00 PM)
 r = requests.get(DOGE_MarToApr + API_key)
 r = r.json()
-with open('DOGE(1)_MarToApr.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/DOGE(1)_MarToApr.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 2: Thursday, April 1, 2021 (1:00:00 PM) - Wednesday, June 23, 2021 (10:00:00 PM)
 r = requests.get(DOGE_AprToJun + API_key)
 r = r.json()
-with open('DOGE(2)_AprToJun.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/DOGE(2)_AprToJun.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 3: Wednesday, June 23, 2021 (3:00:00 PM) - Wednesday, September 15, 2021 (7:00:00 AM)
 r = requests.get(DOGE_JunToSep + API_key)
 r = r.json()
-with open('DOGE(3)_JunToSep.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/DOGE(3)_JunToSep.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 4: Wednesday, September 15, 2021 (7:00:00 AM) - Tuesday, December 7, 2021 (3:00:00 PM)
 r = requests.get(DOGE_SepToDec + API_key)
 r = r.json()
-with open('DOGE(4)_SepToDec.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/DOGE(4)_SepToDec.json','w') as json_file:
     json.dump(r,json_file)
 
 # REQUEST 5: Tuesday December 7 2021 (3:00:00 PM) - Tuesday March 1 2022 (12:00:00 AM)
 r = requests.get(DOGE_DecToMar + API_key)
 r = r.json()
-with open('DOGE(5)_DecToMar.json','w') as json_file:
+with open(ROOT_DIR + '/../data/raw/DOGE(5)_DecToMar.json','w') as json_file:
+    json.dump(r,json_file)
+
+
+''' Shiba Inu '''
+# Links for SHIB (Shiba Inu) historical data
+SHIB_MarToApr = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SHIB&tsym=USD&limit=756&toTs=1617296400'
+SHIB_AprToJun = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SHIB&tsym=USD&limit=2000&toTs=1624500000'
+SHIB_JunToSep = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SHIB&tsym=USD&limit=2000&toTs=1631703600'
+SHIB_SepToDec = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SHIB&tsym=USD&limit=2000&toTs=1638907200'
+SHIB_DecToMar = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SHIB&tsym=USD&limit=2000&toTs=1646110800'
+
+# The five requests for SHIB data are made here, and outputted as json files
+# REQUEST 1: March 1, 2021 - April 1, 2021 (1:00:00 PM)
+r = requests.get(SHIB_MarToApr + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SHIB(1)_MarToApr.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 2: Thursday, April 1, 2021 (1:00:00 PM) - Wednesday, June 23, 2021 (10:00:00 PM)
+r = requests.get(SHIB_AprToJun + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SHIB(2)_AprToJun.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 3: Wednesday, June 23, 2021 (3:00:00 PM) - Wednesday, September 15, 2021 (7:00:00 AM)
+r = requests.get(SHIB_JunToSep + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SHIB(3)_JunToSep.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 4: Wednesday, September 15, 2021 (7:00:00 AM) - Tuesday, December 7, 2021 (3:00:00 PM)
+r = requests.get(SHIB_SepToDec + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SHIB(4)_SepToDec.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 5: Tuesday December 7 2021 (3:00:00 PM) - Tuesday March 1 2022 (12:00:00 AM)
+r = requests.get(SHIB_DecToMar + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SHIB(5)_DecToMar.json','w') as json_file:
+    json.dump(r,json_file)
+
+
+''' Sushi '''
+# Links for SUSHI (Sushi) historical data
+SUSHI_MarToApr = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SUSHI&tsym=USD&limit=756&toTs=1617296400'
+SUSHI_AprToJun = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SUSHI&tsym=USD&limit=2000&toTs=1624500000'
+SUSHI_JunToSep = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SUSHI&tsym=USD&limit=2000&toTs=1631703600'
+SUSHI_SepToDec = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SUSHI&tsym=USD&limit=2000&toTs=1638907200'
+SUSHI_DecToMar = 'https://min-api.cryptocompare.com/data/v2/histohour?fsym=SUSHI&tsym=USD&limit=2000&toTs=1646110800'
+
+# The five requests for SUSHI data are made here, and outputted as json files
+# REQUEST 1: March 1, 2021 - April 1, 2021 (1:00:00 PM)
+r = requests.get(SUSHI_MarToApr + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SUSHI(1)_MarToApr.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 2: Thursday, April 1, 2021 (1:00:00 PM) - Wednesday, June 23, 2021 (10:00:00 PM)
+r = requests.get(SUSHI_AprToJun + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SUSHI(2)_AprToJun.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 3: Wednesday, June 23, 2021 (3:00:00 PM) - Wednesday, September 15, 2021 (7:00:00 AM)
+r = requests.get(SUSHI_JunToSep + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SUSHI(3)_JunToSep.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 4: Wednesday, September 15, 2021 (7:00:00 AM) - Tuesday, December 7, 2021 (3:00:00 PM)
+r = requests.get(SUSHI_SepToDec + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SUSHI(4)_SepToDec.json','w') as json_file:
+    json.dump(r,json_file)
+
+# REQUEST 5: Tuesday December 7 2021 (3:00:00 PM) - Tuesday March 1 2022 (12:00:00 AM)
+r = requests.get(SUSHI_DecToMar + API_key)
+r = r.json()
+with open(ROOT_DIR + '/../data/raw/SUSHI(5)_DecToMar.json','w') as json_file:
     json.dump(r,json_file)
